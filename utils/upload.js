b61eb578d57b84797b9cf277ac9d63ea457af471
王超 <597584896@qq.com>
2019-05-13 13:38:43 +0800

    add mask

--- utils/upload.js
+++ utils/upload.js
@@ -74,3 +74,16 @@ uploader.on('beforeFileQueued', function (file) {
   });
 });
 
+// 点击显示大图
+$('.show-wrap').click(e => {
+  var imgsrc = $('.img-detail').attr('src')
+  if (imgsrc) {
+    $('.big-img').attr('src',imgsrc)
+  } else {
+    return
+  }
+  $('.big-mask').addClass('show-it')
+})
+$('.big-mask').click(e=>{
+  $('.big-mask').removeClass('show-it')
+})
