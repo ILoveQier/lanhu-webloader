fdf3b0270b194acde8807098b4da3290b185055e
王超 <597584896@qq.com>
2019-05-14 13:15:58 +0800

    classify

---
+++ 蓝狐-pc/utils/index.js
@@ -0,0 +1,35 @@
+// 点击显示大图
+$('.show-wrap').click(e => {
+  var imgsrc = $('.img-detail').attr('src')
+  if (imgsrc) {
+    $('.big-img').attr('src',imgsrc)
+  } else {
+    return
+  }
+  $('.big-mask').addClass('show-it')
+})
+$('.show-wrap2').click(e => {
+  var imgsrc = $('.img-detail2').attr('src')
+  if (imgsrc) {
+    $('.big-img').attr('src',imgsrc)
+  } else {
+    return
+  }
+  $('.big-mask').addClass('show-it')
+})
+$('.big-mask').click(e=>{
+  $('.big-mask').removeClass('show-it')
+})
+
+$('.submit').click(e=> {
+  $('.main-container').removeClass('display-flex')
+  $('.main-container').addClass('display-none')
+  $('.content-container').removeClass('display-none')
+  $('.content-container').addClass('display-flex')
+})
+$('.go-back').click(e=> {
+  $('.content-container').removeClass('display-flex')
+  $('.content-container').addClass('display-none')
+  $('.main-container').removeClass('display-none')
+  $('.main-container').addClass('display-flex')
+})
